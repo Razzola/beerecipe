d6efe679267c39ff6d4685b81a3446d12a6f2a7d
Roberto Azzola <r.azzola@reply.it>
2018-06-25 18:55:05 +0200

    t

--- retail/develop2/view.php
+++ retail/develop2/view.php
@@ -125,7 +125,7 @@ if ( isset($_GET['type']) ) {
                             if ( $type == 'ing' ) {
                             
 	                            $mysqli = new mysqli("localhost", "root", "", "beerecipe");
-	                            $result = $mysqli->query("SELECT name, description FROM `ingredients` ORDER BY name");
+	                            $result = $mysqli->query("SELECT name, description, uid FROM `ingredients` ORDER BY name");
 								$row = $result->fetch_row();  
 								                          
                             	while ( $row != null ) {
@@ -134,7 +134,7 @@ if ( isset($_GET['type']) ) {
 	                            <td><?php echo $row[0]; ?></td>
 	                            <td><?php echo $row[1]; ?></td>
 	                            <td>
-									<a href="index.php?p=update&uid=<?php echo $row[0]; ?>&type=<?php echo $type; ?>"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>
+									<a href="index.php?p=update&uid=<?php echo $row[2]; ?>&type=<?php echo $type; ?>"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>
 								</td>
 	                        </tr>
                             <?php
